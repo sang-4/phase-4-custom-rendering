5511a3a1ac1f6fcc327257d12170c5c601a5adbe
Allan <kipropsallan@gmail.com>
2022-09-28 13:37:56 +0300

    Done.

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,7 +1,17 @@
 class Cheese < ApplicationRecord
   
   def summary
-    "#{name}: $#{price}"
+      "#{name}: $#{price}"
+  end
+
+  def show
+      cheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' }, status: :not_found
+  end
+    
   end
 
 end
